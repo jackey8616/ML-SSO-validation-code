db5349ad960c15227e6cc44202bb8081c5cf393c
clooooode <jackey8616@gmail.com>
2018-09-19 09:02:27 +0800

    Add another url for JS fetch to put cookies in body instead of manual set cookie

--- app.py
+++ app.py
@@ -10,17 +10,37 @@ global model
 model = SSOModel()
 model.load()
 
-@app.route('/validationCode', methods=['POST'])
-async def validationCode(request):
+def validateCode(cookies):
     global model
     try:
-        cookies = requests.utils.cookiejar_from_dict(request.cookies)
         for cookie in cookies:
             cookie.domain = 'webapp.yuntech.edu.tw'
         code = model.productionPredict(cookies=cookies)
         return json({ 'success': True, 'code': code })
     except Exception as e:
         return json({ 'fail': True, 'exception': e })
+    
+
+@app.route('/validationCode', methods=['POST'])
+async def validationCode(request):
+    try:
+        print(request.cookies)
+        cookies = requests.utils.cookiejar_from_dict(request.cookies)
+        return validateCode(cookies)
+    except Exception as e:
+        return json({ 'fail': True, 'exception': e })
+
+@app.route('/validationCode/cookieDict', methods=['POST'])
+async def vcCookieDict(request):
+    try:
+        cookies = {}
+        for (key, value) in request.json.items():
+            cookies[key] = value['value']
+        cookies = requests.utils.cookiejar_from_dict(cookies)
+        return validateCode(cookies)
+    except Exception as e:
+        return json({ 'fail': True, 'exception': e })
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
